05b48b5da814dadaafc79639ffe987beb453dd0b
peterlaxalt <pete@laxaltandmciver.co>
2015-12-20 00:54:30 -0600

    front page combined with page.php

--- front-page.php
+++ front-page.php
@@ -1,11 +1,39 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<div class="col-sm-10 gallery-container">
+      <div class="col-sm-12 gallery-item-container">
+          <div class="page-content">
 
-	<?php the_content(); ?>
+						<!-- start wp-loop (page content) -->
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-<?php endwhile; else: ?>
-	<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-<?php endif; ?>
+            <!-- display featured image -->
+            <?php if (has_post_thumbnail( $post->ID ) ): ?>
+              <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+              <img src="<?php echo $image[0]; ?>"  class="img-responsive" /><br />
+            <?php endif; ?>
+
+            <!-- display page title -->
+            <h1 class="pagehead"><?php the_title(); ?></h1><br />
+
+            <!-- display page content -->
+            <p>
+              <?php the_content(); ?>
+            </p>
+
+						<!-- end wp-loop (page content) -->
+				<?php endwhile; else: ?>
+					<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+				<?php endif; ?>
+
+          </div> <!-- /page-content -->
+        </div><!-- /gallery-item-container (inner page wrapper because pete is dumb) -->
+</div><!-- /gallery-container (page wrapper because pete is dumb) -->
+
+    <!-- embellishment (sidebar) -->
+    <div class="col-sm-2 embellish-container" id="stick">
+				<?php get_sidebar(); ?>
+    </div>
+    <!-- /embellishment -->
 
 <?php get_footer(); ?>
